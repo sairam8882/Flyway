55b834b975032bb62063087c46bf3d5212b0fb91
sairam8882 <43983254+sairam8882@users.noreply.github.com>
2018-10-23 15:57:06 +0530

    Delete V8__change.sql

--- V8__change.sql
+++
@@ -1,9 +0,0 @@
--- -----------------------------------------------------
--- Alter email table1 and add constraint foreign keys of people and email_type table.
--- -----------------------------------------------------
-
-alter table email add constraint fk_person_email
-  foreign key (person_id) references people(person_id);
-
-alter table email add constraint fk_email_type
-  foreign key (email_type) references email_type(email_type);
