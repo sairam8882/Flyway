de53d10e4571d87b25b1ff97eaf7f3e77d06d0f3
sairam8882 <43983254+sairam8882@users.noreply.github.com>
2018-10-23 13:08:47 +0530

    Update V6__change.sql

--- V6__change.sql
+++ V6__change.sql
@@ -3,29 +3,6 @@
 -- Create people_view view and people_procedure stored procedure.
 -- -----------------------------------------------------
 
-alter table people add constraint fk_people_state
+alter table people add constraint fk_people_state1
   foreign key (state) references states(state);
 
-create view people_view as (
-  select
-    person_id,
-    first_name,
-    last_name,
-    phone_number,
-    address_line_1,
-    address_line_2,
-    city,
-    state,
-    zip_code
-  from people
-);
-
-DELIMITER //
-CREATE PROCEDURE `people_procedure` (IN var1 INT)
-LANGUAGE SQL
-DETERMINISTIC
-SQL SECURITY DEFINER
-COMMENT 'A procedure'
-BEGIN
-    SELECT var1 + 2 AS result;
-END//
