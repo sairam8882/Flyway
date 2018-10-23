81fb65b028cedfeb46febce3ecc0de8a9ac2c46a
sairam8882 <43983254+sairam8882@users.noreply.github.com>
2018-10-23 15:56:56 +0530

    Delete V6__change.sql

--- V6__change.sql
+++
@@ -1,8 +0,0 @@
--- -----------------------------------------------------
--- Alter people table and add constraint foreign key of state table. 
--- Create people_view view and people_procedure stored procedure.
--- -----------------------------------------------------
-
-alter table people add constraint fk_people_state1
-  foreign key (state) references states(state);
-
